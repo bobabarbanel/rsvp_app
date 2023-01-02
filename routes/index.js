47fbdd5566db0289d40069cc041cca9102f5ba71
bobabarbanel <bob.abarbanel@gmail.com>
2023-01-02 09:57:42 -0800

    add post for contributions

--- routes/index.js
+++ routes/index.js
@@ -50,4 +50,16 @@ router.post("/api/updateRsvp", function(req, res, next) {
 	});
 });
 
+router.post("/api/contribute", function(req, res, next) {
+	let { _id, amount, from } = req.body;
+	const query = { _id: new ObjectId(_id) };
+	collection
+		.updateOne(query, { $set: { contribution: {
+			amount : +amount,
+			from
+		} } })
+		.then(() => {
+			res.json({ response });
+		});
+});
 module.exports = router;
